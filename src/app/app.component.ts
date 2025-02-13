17c1f9231ab705c15a78dd60f301cab8af29f6db
sylvia0530 <vololonandrasanasylvia@gmail.com>
2025-02-13 14:20:14 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { MunicipalitesComponent } from './components/municipalites/municipalites.component';
-import { ManuelsComponent } from './components/manuels/manuels.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,SidebarComponent,TopbarComponent,MunicipalitesComponent],
