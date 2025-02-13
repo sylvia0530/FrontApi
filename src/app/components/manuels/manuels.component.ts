b6d58aaba80582ef8c6345940d49476e668331d4
sylvia0530 <vololonandrasanasylvia@gmail.com>
2025-02-13 14:45:57 +0300

    Update manuels.component.ts

--- src/app/components/manuels/manuels.component.ts
+++ src/app/components/manuels/manuels.component.ts
@@ -2,18 +2,32 @@ import { Component,OnInit} from '@angular/core';
 import { DataService } from '../../service/data.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { Manuel } from '../../manuel';
 @Component({
   selector: 'app-manuels',
-  imports: [DataService,CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './manuels.component.html',
   styleUrl: './manuels.component.css'
 })
 export class ManuelsComponent implements OnInit{
   manuels:any;
+  manuel = new Manuel;
+  constructor(private dataService: DataService) {}
+
   ngOnInit(): void {
     this.getManuelData();
   }
+
   getManuelData(){
     console.log('liste');
+    this.dataService.getManuel().subscribe(res => {
+      this.manuels = res;
+    });
+  }
+
+  insertManuel(){
+    this.dataService.insertManuel(this.manuel).subscribe(res =>{
+      this.getManuelData();
+    });
   }
 }
