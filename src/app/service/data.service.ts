2b6b89e05578eb3facc7806edaaf3db919ea9c34
sylvia0530 <vololonandrasanasylvia@gmail.com>
2025-02-13 14:46:20 +0300

    Update data.service.ts

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -9,17 +9,19 @@ export class DataService {
 
   private apiUrl = 'http://127.0.0.1:8000/api';
 
-  // Méthode pour récupérer les données
   getData(): Observable<any> {
     return this.httpClient.get<any>(`${this.apiUrl}/municipalites`);
   }
 
-  // Méthode pour insérer les données
   insertData(data: any): Observable<any> {
     return this.httpClient.post<any>(`${this.apiUrl}/addmunicipalites`, data);
   }
 
   getManuel(): Observable<any> {
     return this.httpClient.get<any>(`${this.apiUrl}/manuels`);
+  }
+
+  insertManuel(data: any): Observable<any> {
+    return this.httpClient.post<any>(`${this.apiUrl}/addmanuels`, data);
   }
 }
