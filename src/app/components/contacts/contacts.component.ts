4a8ab5a959bcd66bafffc027b20aa88fe1d2cca9
sylvia0530 <vololonandrasanasylvia@gmail.com>
2025-02-17 10:13:05 +0300

    Create contacts.component.ts

---
+++ src/app/components/contacts/contacts.component.ts
@@ -0,0 +1,32 @@
+import { Component,OnInit } from '@angular/core';
+import { DataService } from '../../service/data.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contacts',
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.css'
+})
+export class ContactsComponent implements OnInit{
+  contacts:any;
+  contact = new Contact;
+   constructor(private dataService: DataService) {}
+   ngOnInit(): void {
+     this.getContactData();
+   }
+
+   getContactData(){
+       console.log('liste');
+       this.dataService.getContact().subscribe(res => {
+         this.contacts = res;
+       });
+   }
+
+   insertContact(){
+    this.dataService.insertContact(this.contact).subscribe(res =>{
+      this.getContactData();
+    });
+  }
+}
